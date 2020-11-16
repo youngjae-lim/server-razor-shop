6c550ae9eb810a12187e753897c15b84977f4098
Youngjae <limyoungjae@gmail.com>
2020-11-16 16:17:39 -0500

    Transfered .env variables(Cloudinary variables & Strapi jwt token) to heroku environment variables setting

--- config/server.js
+++ config/server.js
@@ -3,7 +3,8 @@ module.exports = ({ env }) => ({
   port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env("ADMIN_JWT_SECRET", 'f63c7c2d28f5b39721e246b1ca9da84f'),
+      // secret: env("ADMIN_JWT_SECRET", 'f63c7c2d28f5b39721e246b1ca9da84f'),
+      secret: `${process.env.ADMIN_JWT_SECRET}`,
     },
   },
 });
