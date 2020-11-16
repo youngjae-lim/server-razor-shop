c746da9eeb0dc0f578ac8dd682a19e1aca1baeed
Youngjae <limyoungjae@gmail.com>
2020-11-16 15:15:59 -0500

    Changed ADMIN_JWT_SECRET back to the original setting in the ./config/server.js file

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env("ADMIN_JWT_SECRET"),
+      secret: env("ADMIN_JWT_SECRET", 'f63c7c2d28f5b39721e246b1ca9da84f'),
     },
   },
 });
