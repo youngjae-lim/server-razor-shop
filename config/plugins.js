77d89c1103843e3fb4042fdca5c0aa6df7def9a7
Youngjae <limyoungjae@gmail.com>
2020-11-16 18:43:44 -0500

    Updated plugins.js & server.js to distinguish between prod and dev env.

--- config/plugins.js
+++ config/plugins.js
@@ -1,12 +1,27 @@
-module.exports = ({ env }) => ({
-  // ...
-  upload: {
-    provider: "cloudinary",
-    providerOptions: {
-      cloud_name: process.env.CLOUDINARY_NAME,
-      api_key: process.env.CLOUDINARY_KEY,
-      api_secret: process.env.CLOUDINARY_SECRET,
+if (process.env.NODE_ENV === "production") {
+  module.exports = ({ env }) => ({
+    // ...
+    upload: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: process.env.CLOUDINARY_NAME,
+        api_key: process.env.CLOUDINARY_KEY,
+        api_secret: process.env.CLOUDINARY_SECRET,
+      },
     },
-  },
-  // ...
-});
+    // ...
+  });
+} else {
+  module.exports = ({ env }) => ({
+    // ...
+    upload: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+    },
+    // ...
+  });
+}
--- config/server.js
+++ config/server.js
@@ -1,10 +1,21 @@
-module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
-  admin: {
-    auth: {
-      // secret: env("ADMIN_JWT_SECRET", 'f63c7c2d28f5b39721e246b1ca9da84f'),
-      secret: `${process.env.ADMIN_JWT_SECRET}`,
+if (process.env.NODE_ENV === "production") {
+  module.exports = ({ env }) => ({
+    host: env("HOST", "0.0.0.0"),
+    port: env.int("PORT", 1337),
+    admin: {
+      auth: {
+        secret: `${process.env.ADMIN_JWT_SECRET}`,
+      },
     },
-  },
-});
+  });
+} else {
+  module.exports = ({ env }) => ({
+    host: env("HOST", "0.0.0.0"),
+    port: env.int("PORT", 1337),
+    admin: {
+      auth: {
+        secret: env("ADMIN_JWT_SECRET"),
+      },
+    },
+  });
+}
